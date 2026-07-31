2b84f9bbec457cde67a0b17bd092022eef0b2729
André Anderson Feitoza <andre_feitoza@icloud.com>
2026-07-31 18:33:15 -0400

    feat(config): add configuration models and defaults

---
+++ src/ubuntu_ai/config/__init__.py
@@ -0,0 +1,37 @@
+from ubuntu_ai.config.defaults import (
+    APPLICATION_NAME,
+    CONFIG_FILE_NAME,
+    create_default_settings,
+    default_cache_directory,
+    default_config_directory,
+    default_config_file,
+    default_data_directory,
+    default_paths,
+    default_state_directory,
+)
+from ubuntu_ai.config.models import (
+    AIConfig,
+    AppSettings,
+    FeatureConfig,
+    LoggingConfig,
+    PathConfig,
+    UIConfig,
+)
+
+__all__ = [
+    "AIConfig",
+    "APPLICATION_NAME",
+    "AppSettings",
+    "CONFIG_FILE_NAME",
+    "FeatureConfig",
+    "LoggingConfig",
+    "PathConfig",
+    "UIConfig",
+    "create_default_settings",
+    "default_cache_directory",
+    "default_config_directory",
+    "default_config_file",
+    "default_data_directory",
+    "default_paths",
+    "default_state_directory",
+]
